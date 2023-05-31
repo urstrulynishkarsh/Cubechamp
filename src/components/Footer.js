4b9b1b4df2acea445c3e2b0f4a84dc8929c2db91
urstrulynishkarsh <206301010@gkv.ac.in>
2023-05-31 11:43:17 +0530

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -41,45 +41,70 @@ function Footer() {
   }
   return (
     <div>
-<footer
-  className="bg-[#6674cc] relative  ">
-    <div className=" relative flex flex-row  w-12/12 footer-col  max-w-[1280px] gap-10 mx-auto  px-4 ">
-        <div className='flex flex-col mx-auto  mt-20'>
-          <div className='flex flex-row  gap-6 mb-10'>
-          <IoLocationSharp className='w-[28px] h-[28px]'/>
-          <p className=' footer-location'> 30 North Gould Street, Suite R,Sheridan, Wyoming 82801,United States</p>
+    <footer className="bg-[#6674cc] relative">
+      <div className="relative flex flex-row w-11/12 footer-col max-w-[1280px] gap-10 mx-auto ">
+        <div className="flex flex-col mx-auto  mt-20">
+          <div className="flex flex-row gap-6 mb-10">
+            <div className='image'>
+         <IoLocationSharp className="w-[28px] img h-[28px] sm:w-[40px] sm:h-[40px]" />
+         </div>
+            <p className="footer-location  footer-location6 ">
+              30 North Gould Street, Suite R, Sheridan, Wyoming 82801, United States
+            </p>
           </div>
-          <div className='flex flex-row relative gap-6'>
-          <BsTelephoneFill className='w-[28px] h-[28px]'/>
-          <p className='select-none footer-location'> (123) 456-7890</p>
+          <div className="flex flex-row relative object  gap-6">
+            <BsTelephoneFill className="w-[28px] h-[28px]" />
+            <p className="select-none footer-location footer-location6"> (123) 456-7890</p>
           </div>
         </div>
-        <div className='flex flex-col relative social-media-queries   mt-20'>
-          <div className='flex justify-end   gap-8 mb-10  ' >
-            <p className='social-icon mt-1 lining'>Social Media</p>
-            <a href='https://www.facebook.com/profile.php?id=100092532316785' target='_blank'><CiFacebook className='w-[28px] h-[28px]'/></a>
-            <a href='https://www.instagram.com/cubechamps/?igshid=NGExMmI2YTkyZg%3D%3D' target='_blank'><FiInstagram className='w-[28px] h-[28px]'/></a>
-            
+        <div className="flex flex-col relative social-media-queries mt-20">
+          <div className="flex justify-end socialcolumn gap-8 mb-10">
+            <p className="menu-text mt-1 lining">Social Media</p>
+            <div className="flex gap-8">
+              <a
+                href="https://www.facebook.com/profile.php?id=100092532316785"
+                target="_blank"
+              >
+                <CiFacebook className="w-[28px] h-[28px]" />
+              </a>
+              <a
+                href="https://www.instagram.com/cubechamps/?igshid=NGExMmI2YTkyZg%3D%3D"
+                target="_blank"
+              >
+                <FiInstagram className="w-[28px] h-[28px]" />
+              </a>
+            </div>
           </div>
-    
-          <div className='flex flex-row social-media  navlinksfooter gap-16'>
-            <Link to='/' onClick={handleAboutClick}  className='social-icon cursor-pointer lining'>ABOUT US</Link>
-            <Link to='/contact'  onClick={handleClick} className='social-icon cursor-pointer lining'>CONTACT US</Link>
-            <Link to="/privacy" onClick={handleClick2}  className='social-icon cursor-pointer lining'>PRIVACY POLICY</Link>
-            <Link to='/service' onClick={handleClick3} className='social-icon cursor-pointer'>SERVICES</Link>
-
+  
+          <div className="flex flex-row social-media navlinksfooter gap-16">
+            <p id="menu-text" className="menu-text">
+              MENU
+            </p>
+            <Link to="/" onClick={handleAboutClick} className="social-icon cursor-pointer lining">
+              ABOUT US
+            </Link>
+            <Link to="/contact" onClick={handleClick} className="social-icon cursor-pointer lining">
+              CONTACT US
+            </Link>
+            <Link to="/privacy" onClick={handleClick2} className="social-icon cursor-pointer lining">
+              PRIVACY POLICY
+            </Link>
+            <Link to="/service" onClick={handleClick3} className="social-icon cursor-pointer">
+              SERVICES
+            </Link>
           </div>
-
         </div>
-      
-    </div>
-    <div className='flex-1 h-[3px] bg-[#b5cec5] mt-10 w-[1280px] mx-auto'></div>
-    <div  className='relative flex flex-row  w-12/12   max-w-[1280px]  mx-auto  '>
-     
-    <p className='mx-auto mt-6 footer-location footer-location2 '>Copyright © 2023 CubeCHamp.All Rights Reserved.</p>
-    </div>
-</footer>
-</div>
+      </div>
+      <div className="flex-1 h-[3px] bg-[#b5cec5] mt-10 w-[1280px] mx-auto"></div>
+      <div className="relative flex flex-row w-12/12 max-w-[1280px] mx-auto">
+        <p className="mx-auto mt-6 footer-location footer-location2">
+          Copyright © 2023 CubeCHamp.
+          All Rights Reserved.
+        </p>
+      </div>
+    </footer>
+  </div>
+  
   )
 }
 
